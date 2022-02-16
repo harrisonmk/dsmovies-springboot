31066221d33f06ee870f0d5a50d112088b6c9084
harrisonmk <harrison.mitchell@hotmail.com>
2022-02-16 10:16:17 -0400

    projeto criado

---
+++ backend/src/main/java/com/projeto/dsmoviesspringboot/DsmoviesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.dsmoviesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviesSpringbootApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projeto/dsmoviesspringboot/DsmoviesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.dsmoviesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
